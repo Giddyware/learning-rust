1572850c3aa50def3dc8e2699c810cfa29c8bba2
Giddyware <idowugidiyanuoluwa@gmail.com>
2024-04-24 13:13:34 +0100

    chore: variables in rust

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,26 @@
 fn main() {
-    let mut n = 5;
-    n += 1;
+
+    // Variables are immutable by default
+    let  n=5;
+   
     println!("The value of n is: {n}");
+
+    //Scope
+    {
+        let n=6;
+        println!("The value of n in scope: {n}");
+    }
+    // The value of n is: 6
+
+    // Shadowing and return value
+    // let n= {
+    //     6
+    // };
+
+    // Return void
+
+    let n= {
+        let n=3;
+    };
+    println!("The value of n in scope: {n:?}");
 }
