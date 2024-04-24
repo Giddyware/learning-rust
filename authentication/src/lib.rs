be14360fac7be66b78653690eca915238ce6129e
Giddyware <idowugidiyanuoluwa@gmail.com>
2024-04-24 21:35:56 +0100

    chore: build first lib

--- authentication/src/lib.rs
+++ authentication/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn greet_user(name:&str) -> String {
+    format!("Hello {name}")
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_greet_user(){
+        assert_eq!("Hello Adufe", greet_user("Adufe"));
     }
+  
 }
